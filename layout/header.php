268ad5e9333d46dbd6a0af9dd30978c7af1b1eb0
{Thanh Phu} <{phuit2000@gmail.com}>
2023-06-03 09:47:34 +0700

    branch

---
+++ layout/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aperiam at ut veritatis iste accusantium unde nostrum quam porro minima, molestiae tenetur velit inventore reiciendis atque nemo et iure explicabo odit nisi repellendus voluptatibus ducimus natus? Eveniet tempora accusantium enim quis hic voluptate? Illo autem inventore nam eos voluptatum necessitatibus molestiae ducimus molestias facere consequatur adipisci magnam ut animi laboriosam velit explicabo rerum repellendus dolore, corporis ullam, perferendis fuga unde debitis nostrum! Hic possimus autem dignissimos incidunt repellendus magni accusamus pariatur, minus vitae? Quo, dignissimos sunt tempore ut asperiores accusantium magni corrupti placeat rem eius veritatis deleniti sint blanditiis non? </p>
+</body>
+
+</html>
